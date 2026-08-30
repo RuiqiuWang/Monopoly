191f1bb942856f01d61107592c3d06fcb9f3492c
Zhangkai-Ns3 <zk@SQUIRREL-MARSHMALLOW.localdomain>
2026-08-30 15:47:21 +0800

    adding scripts

---
+++ linux_build.sh
@@ -0,0 +1,5 @@
+make clean CC=gcc
+make game_engine CC=gcc
+
+make test CC=gcc
+make json_test CC=gcc
---
+++ win_build.bat
@@ -0,0 +1,3 @@
+mingw32-make game_engine CC=gcc
+mingw32-make test CC=gcc
+mingw32-make json_test CC=gcc
